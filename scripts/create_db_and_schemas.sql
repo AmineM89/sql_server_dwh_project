9c19251225609f4adfa847bd3e7c7ac8cc69ebe5
Amine Mohabeddine <a.mohabeddine@gmail.com>
2025-03-25 13:35:34 +0100

    debug data base creation + comment code

--- scripts/create_db_and_schemas.sql
+++ scripts/create_db_and_schemas.sql
@@ -1,9 +1,19 @@
 USE master;
 
+-- drop the database if already created
+IF EXISTS (SELECT name FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    DROP DATABASE DataWarehouse;
+END
+GO
+
+-- create database
 CREATE DATABASE DataWarehouse;
-
+GO
+--select the created database
 USE DataWarehouse;
-
+GO
+-- create schema for medallion data architecture
 CREATE SCHEMA bronze;
 GO
 CREATE SCHEMA silver;
